8fcbd1d6513aa337eb64ed3d403fdfcf591a3677
Pranavvv1000 <132485339+Pranavvv1000@users.noreply.github.com>
2023-05-10 20:34:55 +0530

    Add files via upload

---
+++ dm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+pip3 install flask
+pip3 install --upgrade Flask
+pip3 install cv2
+pip3 install PIL
+pip3 install flask_sqlalchemy
+pip3 install keras
+pip3 install gunicorn
